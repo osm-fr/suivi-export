9595705de965d63153e6e38ab5f15da76388ce67
sletuffe <git@letuffe.org>
2013-02-20 14:54:19 +0100

    le dossier temporaire pour la génération avait été supprimé

--- exports-cours-eau-longs/exportation-shape.sh
+++ exports-cours-eau-longs/exportation-shape.sh
@@ -3,12 +3,12 @@
 # - exporter le tout en shapefile
 
 CHEMIN_EXPORT="/data/work/osm2pgsql/export-cours-eau"
+CHEMIN_TEMPORAIRE="/data/work/osm2pgsql/tmp"
+mkdir $CHEMIN_TEMPORAIRE 2>/dev/null
 osm_base=`grep pg_france_data_base ../config.php | cut -f2 -d\= | cut -f2 -d\"`
 
-pgsql2shp -f ./tmp/cours_eau_france $osm_base "select \"ref:sandre\",name,st_transform(way,4326) as way from planet_osm_line where \"ref:sandre\" is not null and waterway='river'"
-cd tmp
-tar cvfz $CHEMIN_EXPORT/cours-eau-france.shp.tar.gz cours_eau_france*
-rm -f  cours_eau_france*.shp cours_eau_france*.dbf cours_eau_france*.shx
-cd ..
+pgsql2shp -f $CHEMIN_TEMPORAIRE/cours_eau_france $osm_base "select \"ref:sandre\",name,st_transform(way,4326) as way from planet_osm_line where \"ref:sandre\" is not null and waterway='river'"
+tar cvfz $CHEMIN_EXPORT/cours-eau-france.shp.tar.gz $CHEMIN_TEMPORAIRE/cours_eau_france*
+rm -f  $CHEMIN_TEMPORAIRE/cours_eau_france.*
 
   
