9690fc8b1c3d962e83c092a0173be05396734362
sletuffe <git@letuffe.org>
2013-11-16 16:05:55 +0100

    vu que personne ne passe pas la page d'aide que j'ai écrite, alors j'indique le principal ici même + lien vers la page en question

--- etat-suivi.sh
+++ etat-suivi.sh
@@ -12,15 +12,21 @@ mkdir $chemin_export_administratif 2>/dev/null
 mkdir $chemin_suivi_commune 2>/dev/null
 mkdir $chemin_export_commune 2>/dev/null
 
+cp $d/HEADER.html $chemin_export_commune
+cp $d/HEADER.html $chemin_suivi_commune
+cp $d/HEADER.html $chemin_export_administratif
+
 date_base_monde=`wget $fichier_state_base_monde -q -O - | grep timestamp | sed s/timestamp=// | sed s/[TZ]/" "/g | sed s/\\\\\\\\//g`
 php $d/etat-communes/commune_stats.php $chemin_suivi_commune $chemin_export_commune $pg_serveur_monde $pg_role_monde $pg_password_monde $pg_base_monde $date_base_monde > $chemin_suivi_commune/errors.log 2>&1
 
 #calcul des km par cours d'eau et comparaison au SANDRE
 dossier_cours_eau=$chemin_suivi/longeur-cours-eau-france/
 mkdir $dossier_cours_eau 2>/dev/null
+
 date_base_france=`grep timestamp $fichier_state_base_france | sed s/timestamp=// | sed s/[TZ]/" "/g | sed s/\\\\\\\\//g`
 php $d/longeur-cours-eau-france/suivi-cours-eau.php $pg_base_france $date_base_france > $dossier_cours_eau/comparaison-sandre.html
 cp $d/longeur-cours-eau-france/sorttable.js $dossier_cours_eau
+cp $d/HEADER.html $dossier_cours_eau
 
 # Export des régions et départements
 $d/exports-administratif/export-limites-administratives.sh 6 96 departements $chemin_export_administratif $pg_base_france
