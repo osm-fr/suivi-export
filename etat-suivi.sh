ca591c3a026b1a71ef9e0e0aeb7a51dda83a7a75
sletuffe <git@letuffe.org>
2013-12-12 01:27:18 +0100

    export départements/régions avec une base monde (inclusion des dom/rom)

--- etat-suivi.sh
+++ etat-suivi.sh
@@ -12,15 +12,22 @@ mkdir $chemin_export_administratif 2>/dev/null
 mkdir $chemin_suivi_commune 2>/dev/null
 mkdir $chemin_export_commune 2>/dev/null
 
-cp $d/HEADER.html $chemin_export_commune
 cp $d/HEADER.html $chemin_suivi_commune
 cp $d/HEADER.html $chemin_export_administratif
 
 #ouais clair, factorisation de factorisation, ça fini en usine à gaz...
-param_base_monde="$pg_serveur_monde $pg_serveur_port_monde $pg_role_monde $pg_password_monde $pg_base_monde $table_qui_contient_la_france $table_qui_contient_la_france"
+param_base_monde="$pg_serveur_monde $pg_serveur_port_monde $pg_role_monde $pg_password_monde $pg_base_monde $table_qui_contient_la_france $id_france_dans_cette_table"
 
-date_base_monde="`wget $fichier_state_base_monde -q -O - | grep timestamp | sed s/timestamp=// | sed s/[TZ]/" "/g | sed s/\\\\\\\\//g`"
+#export et suivi des communes
+cat $d/HEADER.html | sed s/@@date@@/"$date_base_monde"/g > $chemin_export_commune/HEADER.html
 php $d/etat-communes/commune_stats.php $chemin_suivi_commune $chemin_export_commune $param_base_monde "$date_base_monde" > $chemin_suivi_commune/errors.log 2>&1
+
+# Export des régions et départements
+date_base_monde="`wget $fichier_state_base_monde -q -O - | grep timestamp | sed s/timestamp=// | sed s/[TZ]/" "/g | sed s/\\\\\\\\//g`"
+cat $d/HEADER.html | sed s/@@date@@/"$date_base_monde"/g > $chemin_export_commune/HEADER.html
+$d/exports-administratif/export-limites-administratives.sh 6 101 departements $chemin_export_administratif $param_base_monde
+$d/exports-administratif/export-limites-administratives.sh 4 27 regions $chemin_export_administratif $param_base_monde
+
 
 
 #calcul des km par cours d'eau et comparaison au SANDRE
@@ -30,9 +37,6 @@ mkdir $dossier_cours_eau 2>/dev/null
 param_base_france="$pg_serveur_france $pg_serveur_port_france $pg_role_france $pg_password_france $pg_base_france"
 date_base_france="`wget $fichier_state_base_france -q -O - | grep timestamp | sed s/timestamp=// | sed s/[TZ]/" "/g | sed s/\\\\\\\\//g`"
 
-# Export des régions et départements
-$d/exports-administratif/export-limites-administratives.sh 6 96 departements $chemin_export_administratif $param_base_france
-$d/exports-administratif/export-limites-administratives.sh 4 22 regions $chemin_export_administratif $param_base_france
 
 php $d/longeur-cours-eau-france/suivi-cours-eau.php $pg_serveur_france $pg_serveur_port_france $pg_role_france $pg_password_france $pg_base_france "$date_base_france" > $dossier_cours_eau/comparaison-sandre.html
 cp $d/longeur-cours-eau-france/sorttable.js $dossier_cours_eau
--- exports-administratif/export-limites-administratives.sh
+++ exports-administratif/export-limites-administratives.sh
@@ -2,7 +2,7 @@
 dossier_temporaire="/dev/shm/tmp"
 mkdir $dossier_temporaire 2>/dev/null
 
-if [ a$8 = "a" ] ;  then
+if [ a${10} = "a" ] ;  then
 echo "Utilisation : ./export-limites-administratives.sh <admin_level> <nombre_attendu> <nom du fichier> <dossier où exporter> <host base pg> <user pg> <password pg> <pg dbname>"
 echo ""
 echo "Où <admin_level> vaut 6 pour les départements, 4 pour les regions"
@@ -10,20 +10,20 @@ echo "Où <nombre_attendu> et le nombre de contours attendu pour déterminer si 
 echo "Où <nom du fichier> et le nom souhaité pour le fichier d'archive et des shapefiles"
 echo "Où <dossier où exporter> est le dossier dans lequel seront créér les shapefiles"
 echo "Où <nom de la base locale> est le nom de la base postgresql local, au schéma osm2pgsql contenant la france"
-echo "Exemple : ./export-limites-administratives.sh 6 96 departements"
-echo "Exemple : ./export-limites-administratives.sh 4 22 regions"
+echo "Exemple : ./export-limites-administratives.sh 6 101 departements"
+echo "Exemple : ./export-limites-administratives.sh 4 27 regions"
 
 exit
 fi
 
 # Bidouille car les regions on comme référence principale ref:INSEE, mais les départements ref
 if  [ $1 == 4 ] ; then
-  champ_contenant_ref="ref:INSEE"
+  champ_contenant_ref="tags->'ref:INSEE'"
 else
   champ_contenant_ref="ref"
 fi
 
-pgsql2shp -h $5 -p $6 -u $7 -P $8 -f $dossier_temporaire/$3-metropole $9 "select st_transform(admin.way,4326) as way,admin.name as nom,\"$champ_contenant_ref\" as numero from planet_osm_polygon as admin where admin.admin_level='$1' and admin.\"$champ_contenant_ref\" is not null and isvalid(admin.way)='t';" > $dossier_temporaire/resultat 2>&1
+pgsql2shp -h $5 -p $6 -u $7 -P $8 -f $dossier_temporaire/$3-metropole $9 "select st_transform(admin.way,4326) as way,admin.name as nom,$champ_contenant_ref as numero from planet_osm_polygon as admin,${10} as f where f.id=${11} and st_within(ST_PointOnSurface(admin.way),f.way) and admin_level='$1' and st_isvalid(admin.way)='t';" > $dossier_temporaire/resultat 2>&1
 RES=`cat $dossier_temporaire/resultat | grep "\[$2"` 
 
 if [ "a$RES" = "a" ] ; then
